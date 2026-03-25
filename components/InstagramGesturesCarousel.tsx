dd9f26a34697c0580fac52732f4b520b066c7629
Fer F <fer@Fer-Mac-Black.local>
2026-03-25 14:17:40 -0300

    fix: use only 12 unique images, remove duplicates completely
    - Confirmed only 12 unique images exist (insta-1.png to insta-12.png)
    - Removed all duplicate references
    - Simplified getImagePath to use only /img/ folder
    - Now showing 12 unique images without any duplicates
    - Maintains randomization for variety

--- components/InstagramGesturesCarousel.tsx
+++ components/InstagramGesturesCarousel.tsx
@@ -6,12 +6,7 @@ import { Instagram, X, ZoomIn, Share2 } from "lucide-react";
 import Image from "next/image";
 
 // Use only confirmed unique images from img folder
-const baseInstagramImages = [
-  // 12 images from /img/
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-  // 3 images from /public/ (using different numbering to avoid conflicts)
-  101, 102, 103
-];
+const baseInstagramImages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const getImageExtension = (num: number): string => {
   // JPEG images are 15-21
@@ -19,11 +14,6 @@ const getImageExtension = (num: number): string => {
 };
 
 const getImagePath = (num: number): string => {
-  if (num >= 101 && num <= 103) {
-    // Images from /public/ folder
-    return `/insta-${num - 100}.png`;
-  }
-  // Images from /img/ folder
   return `/img/insta-${num}${getImageExtension(num)}`;
 };
 
